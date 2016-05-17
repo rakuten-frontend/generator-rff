e47cb362184d930dc287999de2889cfdb48a7bd9
Hiroyuki Tanjo <expheno@gmail.com>
2016-05-17 09:50:32 +0900

    Revert "Update yeoman-generator to 0.23.3"
    This reverts commit 13a0fadc13c6bf214d9ba558445b9cbca3ca4e79.

--- generators/app/index.js
+++ generators/app/index.js
@@ -41,6 +41,7 @@ module.exports = generators.Base.extend({
   prompting: {
     // Setup prompting type
     askForType: function () {
+      var done = this.async();
       var questions = [];
       var choices = [
         {
@@ -79,35 +80,37 @@ module.exports = generators.Base.extend({
         });
       }
 
-      return this.prompt(questions)
-        .then(function (answers) {
-          if (typeof answers.configType !== 'undefined') {
-            this.configType = answers.configType;
-          }
+      this.prompt(questions, function (answers) {
+        if (typeof answers.configType !== 'undefined') {
+          this.configType = answers.configType;
+        }
 
-          // Preset for skipping prompt
-          switch (this.configType) {
-            case 'standard':
-              this.preset = standardPreset;
-              break;
-            case 'minimum':
-              this.preset = minimumPreset;
-              break;
-            case 'user':
-              this.preset = this.config.getAll();
-              break;
-            case 'custom':
-              this.preset = {};
-              break;
-          }
+        // Preset for skipping prompt
+        switch (this.configType) {
+          case 'standard':
+            this.preset = standardPreset;
+            break;
+          case 'minimum':
+            this.preset = minimumPreset;
+            break;
+          case 'user':
+            this.preset = this.config.getAll();
+            break;
+          case 'custom':
+            this.preset = {};
+            break;
+        }
 
-          // Initial settings
-          this.settings = _.assign({}, defaultConfig, this.config.getAll());
-        }.bind(this));
+        // Initial settings
+        this.settings = _.assign({}, defaultConfig, this.config.getAll());
+
+        done();
+      }.bind(this));
     },
 
     // Project settings
     askFor: function () {
+      var done = this.async();
       var questions = [];
 
       // Project name
@@ -158,16 +161,17 @@ module.exports = generators.Base.extend({
         });
       }
 
-      return this.prompt(questions)
-        .then(function (answers) {
-          // Finalize generator settings
-          this.settings.name = typeof answers.name !== 'undefined' ? answers.name : this.preset.name;
-          this.store = answers.store || false;
-          options.forEach(function (option) {
-            var name = option.name;
-            this.settings[name] = answers[name] || this.preset[name];
-          }.bind(this));
+      this.prompt(questions, function (answers) {
+        // Finalize generator settings
+        this.settings.name = typeof answers.name !== 'undefined' ? answers.name : this.preset.name;
+        this.store = answers.store || false;
+        options.forEach(function (option) {
+          var name = option.name;
+          this.settings[name] = answers[name] || this.preset[name];
         }.bind(this));
+
+        done();
+      }.bind(this));
     }
   },
 
